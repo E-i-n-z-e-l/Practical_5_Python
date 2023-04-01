3dcd425c5737a870701133612b077ed086812540
Einzel <einzel0102@mail.ru>
2023-04-02 00:25:37 +0800

    Группировка слов с помощтю словаря и множеств. Задача из собеседования.

---
+++ Task_interview03/Task_interview03.py
@@ -0,0 +1,17 @@
+# Sample Input ["eat", "tea", "tan", "ate", "nat", “bat"] Sample Output [ ["ate", "eat", "tea"], ["nat", "tan"], ["bat"] ] Т . е . сгруппировать слова по " общим буквам ". 
+
+def group_letter(input_list):
+	word_dict = {}
+	for word in input_list:
+		if (frozenset(word), len(word)) not in word_dict:
+			word_dict[(frozenset(word), len(word))] = [word]
+		else:
+			word_dict[(frozenset(word), len(word))].append(word)
+	res_list = []
+	for value in word_dict.values():
+		res_list.append(value)
+	return res_list
+
+print(group_letter(['eat', 'tea', 'tan', 'ate', 'nat', 'bat', 'batt', 'b', 'cadfsdf']))
+
+
